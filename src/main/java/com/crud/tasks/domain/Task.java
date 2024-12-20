f6583289e378fd3b0dede779e3b66c1107311e7e
Jareczeq <contact@jareczeq.pl>
2024-12-20 21:50:34 +0100

    Zrobiono zadanie

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+
+    private Long id;
+    private String title;
+    private String content;
+
+}
